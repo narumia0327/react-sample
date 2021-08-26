cdaf477553e32bf1d2194fe944ea27b15f87f1e6
narumia0327 <73158256+narumia0327@users.noreply.github.com>
2021-08-26 22:13:37 +0900

    更新したよ

--- main.tsx
+++ main.tsx
@@ -1,7 +1,26 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
+import { css } from "@emotion/css";
+
+const App = (props: { count: number }): React.ReactElement => {
+  return (
+    <div
+      className={css({
+        display: "grid",
+        gap: 8,
+      })}
+    >
+      <Card count={1} />
+      <Card count={2} />
+    </div>
+  );
+};
+
+const Card = (props: { count: number }): React.ReactElement => {
+  return <div>{props.count}</div>;
+};
 
 const entryElement = document.createElement("div");
 document.body.append(entryElement);
 
-ReactDom.render(<div>Reactだよ</div>, entryElement);
+ReactDom.render(<App count={2} />, entryElement);
